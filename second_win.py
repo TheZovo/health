ae2ca7519f3ed795cad97ed68154c49e69e283d4
thezovo <vampirs.alucard@gmail.com>
2025-04-17 20:08:37 +0300

    c

--- second_win.py
+++ second_win.py
@@ -2,7 +2,7 @@ from instr import *
 from final_win import *
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QLineEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QLineEdit, QHBoxLayout
 
 class TestWin(QWidget):
     def __init__(self):
@@ -31,6 +31,32 @@ class TestWin(QWidget):
         self.line_name = QLineEdit(txt_hintname)
         self.line_age = QLineEdit(txt_hintage)
         self.line_name = QLineEdit(txt_hintname)
+        self.line_test1 = QLineEdit(txt_hinttest1)
+        self.line_test2 = QLineEdit(txt_hinttest2)
+        self.line_test3 = QLineEdit(txt_hinttest3)
+        
+        self.l_line = QVBoxLayout()
+        self.r_line = QVBoxLayout()
+        self.h_line = QHBoxLayout()
+        self.r_line.addWidget(self.text_timer, alignment=Qt.AlignCenter)
+        self.r_line.addWidget(self.text_name, alignment=Qt.AlignLeft)
+        self.r_line.addWidget(self.line_name, alignment=Qt.AlignLeft)
+        self.r_line.addWidget(self.text_age, alignment=Qt.AlignLeft)
+        self.r_line.addWidget(self.line_age, alignment=Qt.AlignLeft)
+        self.r_line.addWidget(self.text_test1, alignment=Qt.AlignLeft)
+        self.r_line.addWidget(self.btn_test1, alignment=Qt.AlignLeft)
+        self.r_line.addWidget(self.line_test1, alignment=Qt.AlignLeft)
+        self.r_line.addWidget(self.text_test2, alignment=Qt.AlignLeft)
+        self.r_line.addWidget(self.btn_test2, alignment=Qt.AlignLeft)
+        self.r_line.addWidget(self.text_test3, alignment=Qt.AlignLeft)
+        self.r_line.addWidget(self.btn_test3, alignment=Qt.AlignLeft)
+        self.r_line.addWidget(self.line_test2, alignment=Qt.AlignLeft)
+        self.r_line.addWidget(self.line_test3, alignment=Qt.AlignLeft)
+        self.r_line.addWidget(self.btn_next, alignment=Qt.AlignCenter)
+        self.h_line.addLayout(self.l_line)
+        self.h_line.addLayout(self.r_line)
+        self.setLayout(self.h_line)
+
 
 
     def next_click(self):
